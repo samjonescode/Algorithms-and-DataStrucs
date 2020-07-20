f3c4b6ddcdcc2932d594c385d29826aaba8c32c3
Sam Jones <sam.jones@latch.com>
2020-07-19 21:51:30 -0400

    added sorting

---
+++ Java/Sorting/MergeSort.java
@@ -0,0 +1,56 @@
+package Java.Sorting;
+
+public class MergeSort {
+
+    public static void main(String[] args) {
+        int[] nums = { 1, -9, 2, 4, -20, 3, 30, 20, 11, 203 };
+
+        mergesort(nums);
+
+        for (int i : nums) {
+            System.out.println(i);
+        }
+    }
+
+    static void mergesort(int[] array) {
+        int[] helper = new int[array.length];
+
+        mergesort(array, helper, 0, array.length - 1);
+
+    }
+
+    static void mergesort(int[] array, int[] helper, int low, int high) {
+        if (low < high) {
+            int middle = (low + high) / 2;
+            mergesort(array, helper, low, middle);
+            mergesort(array, helper, middle + 1, high);
+            merge(array, helper, low, middle, high);
+        }
+    }
+
+    static void merge(int[] array, int[] helper, int low, int middle, int high) {
+        for (int i = low; i <= high; i++) {
+            helper[i] = array[i];
+        }
+
+        int helperLeft = low;
+        int helperRight = high;
+        int current = low;
+
+        while (helperLeft <= middle && helperRight <= high) {
+            if (helper[helperLeft] <= helper[helperRight]) {
+                array[current] = helper[helperLeft];
+                helperLeft++;
+            } else {
+                array[current] = helper[helperLeft];
+                helperRight++;
+            }
+            current++;
+        }
+
+        int remaining = middle - helperLeft;
+        for (int i = 0; i <= remaining; i++) {
+            array[current + i] = helper[helperLeft + i];
+        }
+    }
+}
---
+++ Java/Sorting/Quicksort.java
@@ -0,0 +1,49 @@
+package Java.Sorting;
+
+public class Quicksort {
+
+    public static void main(String[] args) {
+        int[] nums = { 1, -9, 2, 4, -20, 3, 30, 20, 11, 203 };
+        quickSort(nums, 0, nums.length - 1);
+
+        for (int i : nums) {
+            System.out.println(i);
+        }
+    }
+
+    static void quickSort(int[] arr, int left, int right) {
+        int index = partition(arr, left, right);
+        if (left < index - 1) {
+            quickSort(arr, left, index - 1);
+        }
+        if (index < right) {
+            quickSort(arr, index, right);
+        }
+    }
+
+    static int partition(int[] arr, int left, int right) {
+        int pivot = arr[(left + right) / 2];
+        while (left <= right) {
+            while (arr[left] < pivot)
+                left++;
+
+            while (arr[right] > pivot)
+                right--;
+
+            if (left <= right) {
+                swap(arr, left, right);
+                left++;
+                right--;
+            }
+        }
+
+        return left;
+    }
+
+    static void swap(int[] arr, int left, int right) {
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+    }
+
+}
